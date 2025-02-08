3e79beca679a9c88e67fe67ad5f1a531452d615f
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2025-02-08 19:20:55 +0100

    add dialog component for withdrawal details in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,19 @@
+import { DialogComponent } from "@/components/DialogComponent";
+import WithdrawalDetails from "@/components/DialogComponent/WithdrawalDetails";
+
 export default function DashboardPage() {
   return (
     <div className="text-black">
       <h2 className="text-2xl font-bold">Welcome to the Admin Dashboard</h2>
       <p className="mt-2">Manage users, settings, and more.</p>
+      <div className="flex">
+        <DialogComponent
+          title="Dialog Title"
+          trigger={<button>Open Dialog</button>}
+        >
+          <WithdrawalDetails />
+        </DialogComponent>
+      </div>
     </div>
   );
 }
