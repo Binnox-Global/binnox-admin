aad353f99286319f71d92f41f2540f4b4da57c02
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2025-02-12 15:39:09 +0100

    refactor DialogComponent to utilize DialogContext for state management and improve styling

--- src/components/DialogComponent/index.tsx
+++ src/components/DialogComponent/index.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import { Dialog2CloseSvg, DialogCloseSvg } from "../icons/DialogCloseSvg";
+import "./style.scss";
+import { useDialog } from "@/contexts/DialogContext";
 
 interface DialogComponentProps {
   children: React.ReactNode;
@@ -13,16 +15,17 @@ export function DialogComponent({
   title,
   children,
 }: DialogComponentProps) {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+  const { isOpen, toggleDialog } = useDialog();
   return (
     <div className="DialogComponent">
-      <div onClick={() => setIsOpen(!isOpen)}>{trigger}</div>
+      <div onClick={() => toggleDialog("open")}>{trigger}</div>
       {isOpen && (
-        <div className=" absolute top-0 left-0 right-0 min-h-full  z-[8] flex justify-center items-center py-[110px] pb -[50px] bg-black/40 backdrop-blur-[30px] border border-red-500">
-          <div className="relative border border-red-500">
+        <div className="Dialog-background bg-black/40 backdrop-blur-[30px]">
+          <div className="relative p b-20 border border-red-500 z-[20]">
             <div
-              className="close-button cursor-pointer absolute top-[22px] right-[23px] p-2 z-[10] "
-              onClick={() => setIsOpen(false)}
+              className="close-button cursor-pointer absolute top-[22px] right-[23px] p-2 z-[10]"
+              onClick={() => toggleDialog(null)}
             >
               <DialogCloseSvg />
             </div>
@@ -33,5 +36,3 @@ export function DialogComponent({
     </div>
   );
 }
-
-//  default DialogComponent;
