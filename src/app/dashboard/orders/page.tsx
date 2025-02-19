a3e4cb09f86e14da9e12582e12e44b828fcab40b
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2025-02-19 13:19:35 +0100

    feat: add OrderPage and SendPage components with stats cards and order lists

---
+++ src/app/dashboard/orders/page.tsx
@@ -0,0 +1,51 @@
+import { StatsCard } from "@/components/cards/StatsCard";
+import { FileSvg } from "@/components/icons/FileSvg";
+import { OrderList } from "@/components/tables/OrderList";
+import React from "react";
+
+function OrderPage() {
+  return (
+    <div className="flex flex-col gap-[50px] text-black">
+      {/* Regular Stats Cards Row */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <StatsCard
+          title="Total Revenue"
+          value="₦95,324"
+          percentage={16}
+          timeFrame="₦45,000 today"
+          icon={<FileSvg className="text-[#F46702]" size={24} />}
+        />
+
+        <StatsCard
+          title="Total Orders"
+          value="902"
+          percentage={2}
+          timeFrame="today"
+          icon={<FileSvg className="text-[#F46702]" size={24} />}
+        />
+
+        <StatsCard
+          title="Customers"
+          value="1278"
+          percentage={-5}
+          timeFrame="today"
+          icon={<FileSvg className="text-[#F46702]" size={24} />}
+        />
+
+        <StatsCard
+          title="Riders Online"
+          value="12"
+          percentage={1}
+          timeFrame="today"
+          icon={<FileSvg className="text-[#F46702]" size={24} />}
+        />
+      </div>
+
+      <div className="">
+        <OrderList />
+      </div>
+    </div>
+  );
+}
+
+export default OrderPage;
