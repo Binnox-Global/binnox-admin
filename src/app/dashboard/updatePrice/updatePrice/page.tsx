982866367a63a800d1c9478aa97762fa13052242
Josiah Victor <62908527+jovick-coder@users.noreply.github.com>
2025-02-26 18:16:49 +0100

    feat: add page component for updating product prices with NewProduct dialog

---
+++ src/app/dashboard/updatePrice/updatePrice/page.tsx
@@ -0,0 +1,13 @@
+import NewProduct from '@/components/DialogComponent/NewProduct'
+import React from 'react'
+
+function page() {
+  return (
+    <div className="flex justify-center">
+
+      <NewProduct />
+    </div>
+  )
+}
+
+export default page
